947c883c179a09a86bf5569d5673ee1ff615c206
Vinicius Arcanjo <vindasil@fiu.edu>
2023-08-18 15:37:39 -0300

    chore: replaced kytos_api_helpers with async functions add support for retrying

--- kytos_api_helper.py
+++ kytos_api_helper.py
@@ -2,92 +2,63 @@
 other kytos napps' APIs """
 
 
-import datetime
+from collections import defaultdict
+from typing import Iterable
 
 import httpx
+from tenacity import (
+    retry,
+    retry_if_exception_type,
+    stop_after_attempt,
+    wait_random,
+    wait_combine,
+    wait_fixed,
+)
+
+from kytos.core.retry import before_sleep
 from napps.kytos.telemetry_int import settings
 
-from kytos.core import log
 
-from .settings import flow_manager_api, mef_eline_api
-
-# pylint: disable=fixme,too-many-arguments,no-else-return
-
-
-def kytos_api(
-    get=False,
-    put=False,
-    post=False,
-    mef_eline=False,
-    evc_id=None,
-    flow_manager=False,
-    switch=None,
-    data=None,
-    metadata=False,
-):
-    """Main function to handle requests to Kytos API."""
-
-    # TODO: add support for batch, temporizer, retries
-    if flow_manager_api:
-        kytos_api_url = flow_manager_api
-    if mef_eline:
-        kytos_api_url = mef_eline_api
-
-    headers = {"Content-Type": "application/json"}
-
-    try:
-        if get:
-            if data:
-                kytos_api_url += data
-            return httpx.get(kytos_api_url, timeout=10).json()
-
-        elif put:
-            httpx.put(kytos_api_url, timeout=10, headers=headers)
-
-        elif post:
-            if mef_eline and metadata:
-                url = f"{kytos_api_url}/evc/{evc_id}/metadata"
-                response = httpx.post(url, headers=headers, json=data, timeout=10)
-                return response.status_code == 201
-
-            if flow_manager:
-                url = f"{kytos_api_url}/{switch}"
-                response = httpx.post(url, headers=headers, json=data, timeout=10)
-                return response.status_code == 202
-
-    except httpx.RequestError as http_err:
-        log.error(f"HTTP error occurred: {http_err}")
-
-    return False
-
-
-def get_evcs(archived="false") -> dict:
+@retry(
+    stop=stop_after_attempt(5),
+    wait=wait_combine(wait_fixed(3), wait_random(min=2, max=7)),
+    before_sleep=before_sleep,
+    retry=retry_if_exception_type((httpx.RequestError, httpx.HTTPStatusError)),
+)
+async def get_evcs(archived="false") -> dict:
     """Get EVCs."""
-    try:
-        response = httpx.get(f"{settings.mef_eline_api}/evc/?archived={archived}")
+    async with httpx.AsyncClient(base_url=settings.mef_eline_api) as client:
+        response = await client.get(f"/evc/?archived={archived}", timeout=10)
         response.raise_for_status()
         return response.json()
-    except httpx.HTTPStatusError as exc:
-        log.error(f"response: {response.text} for {str(exc)}")
-        return {}
 
 
-def get_evc(evc_id: str) -> dict:
+@retry(
+    stop=stop_after_attempt(5),
+    wait=wait_combine(wait_fixed(3), wait_random(min=2, max=7)),
+    before_sleep=before_sleep,
+    retry=retry_if_exception_type((httpx.RequestError, httpx.HTTPStatusError)),
+)
+async def get_evc(evc_id: str) -> dict:
     """Get EVC."""
-    try:
-        response = httpx.get(f"{settings.mef_eline_api}/evc/{evc_id}")
+    async with httpx.AsyncClient(base_url=settings.mef_eline_api) as client:
+        response = await client.get(f"/evc/{evc_id}", timeout=10)
         if response.status_code == 404:
             return {}
 
         response.raise_for_status()
         data = response.json()
         return {data["id"]: data}
-    except httpx.HTTPStatusError as exc:
-        log.error(f"response: {response.text} for {str(exc)}")
-        return {}
 
 
-def get_evc_flows(cookie: int, *dpid: str) -> dict:
+# TODO eventually remove it too
+@retry(
+    stop=stop_after_attempt(5),
+    wait=wait_combine(wait_fixed(3), wait_random(min=2, max=7)),
+    before_sleep=before_sleep,
+    retry=retry_if_exception_type((httpx.RequestError, httpx.HTTPStatusError)),
+)
+async def get_evc_flows(cookie: int, *dpid: str) -> dict:
     """Get EVC's flows given a range of cookies."""
     endpoint = (
         f"stored_flows?cookie_range={cookie}&cookie_range={cookie}"
@@ -96,39 +67,67 @@ def get_evc_flows(cookie: int, *dpid: str) -> dict:
     if dpid:
         dpid_query_args = [f"&dpid={val}" for val in dpid]
         endpoint = f"{endpoint}{''.join(dpid_query_args)}"
-    response = httpx.get(f"{settings.flow_manager_api}/{endpoint}")
-    try:
+    async with httpx.AsyncClient(base_url=settings.flow_manager_api) as client:
+        response = await client.get(f"/{endpoint}", timeout=10)
         response.raise_for_status()
         return response.json()
-    except httpx.HTTPStatusError as exc:
-        log.error(f"response: {response.text} for {str(exc)}")
-        return {}
 
 
-def set_telemetry_metadata_true(evc_id, direction):
-    """Set telemetry enabled metadata item to true"""
-    data = {
-        "telemetry": {
-            "enabled": True,
-            "direction": direction,
-            "timestamp": datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S"),
-        }
-    }
-    return kytos_api(post=True, mef_eline=True, evc_id=evc_id, metadata=True, data=data)
+@retry(
+    stop=stop_after_attempt(5),
+    wait=wait_combine(wait_fixed(3), wait_random(min=2, max=7)),
+    before_sleep=before_sleep,
+    retry=retry_if_exception_type((httpx.RequestError, httpx.HTTPStatusError)),
+)
+async def get_stored_flows(
+    cookies: list[int] = None,
+) -> dict[int, list[dict]]:
+    """Get flow_manager stored_flows grouped by cookies given a list of cookies."""
+    cookies = cookies or []
+
+    cookie_range_args = []
+    for cookie in cookies:
+        # gte cookie
+        cookie_range_args.append(f"cookie_range={cookie}")
+        # lte cookie
+        cookie_range_args.append(f"cookie_range={cookie}")
+
+    endpoint = "stored_flows?state=installed&state=pending"
+    if cookie_range_args:
+        endpoint = f"{endpoint}&{'&'.join(cookie_range_args)}"
+
+    async with httpx.AsyncClient(base_url=settings.flow_manager_api) as client:
+        response = await client.get(f"/{endpoint}", timeout=10)
+        response.raise_for_status()
+        return _map_stored_flows_by_cookies(response.json())
 
 
-def set_telemetry_metadata_false(evc_id):
-    """Set telemetry enabled metadata item to false"""
-    data = {
-        "telemetry": {
-            "enabled": False,
-            "timestamp": datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S"),
-        }
-    }
+def _map_stored_flows_by_cookies(stored_flows: dict) -> dict[int, list[dict]]:
+    """Map stored flows by cookies.
 
-    return kytos_api(post=True, mef_eline=True, evc_id=evc_id, metadata=True, data=data)
+    This is for mapping the data by cookies, just to it can be
+    reused upfront by bulk operations.
+    """
+    flows_by_cookies = defaultdict(list)
+    for dpid, flows in stored_flows.items():
+        for flow in flows:
+            flows_by_cookies[flow["flow"]["cookie"]].append(flow)
+    return flows_by_cookies
 
 
-def kytos_push_flows(switch, data):
-    """Push flows to Flow Manager"""
-    return kytos_api(post=True, flow_manager=True, switch=switch, data=data)
+@retry(
+    stop=stop_after_attempt(5),
+    wait=wait_combine(wait_fixed(3), wait_random(min=2, max=7)),
+    before_sleep=before_sleep,
+    retry=retry_if_exception_type((httpx.RequestError, httpx.HTTPStatusError)),
+)
+async def add_evcs_metadata(evcs: Iterable[str], new_metadata: dict) -> dict:
+    """Add EVC metadata."""
+    async with httpx.AsyncClient(base_url=settings.mef_eline_api) as client:
+        response = await client.post(
+            "/evc/metadata",
+            timeout=10,
+            json={**new_metadata, **{"circuit_ids": [evc_id for evc_id in evcs]}},
+        )
+        response.raise_for_status()
+        return response.json()
