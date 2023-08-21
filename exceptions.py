73f852059fb4a1c18e24238b55fe1882599c3ba1
Vinicius Arcanjo <vindasil@fiu.edu>
2023-08-21 17:10:27 -0300

    chore: updated exceptions

--- exceptions.py
+++ exceptions.py
@@ -20,24 +20,67 @@ class ErrorBase(Exception):
 
 
 class NoProxyPortsAvailable(ErrorBase):
-    """Exception in case an UNI doesn't have a proxy port configured or operational.
-    That's a warning."""
+    """Exception in case an UNI doesn't have a proxy port configured or operational."""
 
-    def __init__(self, evc_id, message="no proxy ports available."):
+    def __init__(self, evc_id, message="no proxy ports available"):
         super().__init__(evc_id, message)
 
 
-class EvcHasNoINT(ErrorBase):
-    """Exception in case the EVC doesn't have INT/telemetry enabled but it was
-    treated as if. That's a warning."""
+class ProxyPortError(ErrorBase):
 
-    def __init__(self, evc_id, message="INT was not enabled for this EVC."):
+    """ProxyPortError."""
+
+    def __init__(self, evc_id: str, msg: str) -> None:
+        """Constructor of ProxyPortError."""
+        super().__init__(evc_id, msg)
+
+
+class ProxyPortNotFound(ProxyPortError):
+    """ProxyPortNotFound."""
+
+    def __init__(self, evc_id: str, msg: str) -> None:
+        """Constructor of ProxyPortError."""
+        super().__init__(evc_id, msg)
+
+
+class ProxyPortStatusNotUP(ProxyPortError):
+    """ProxyPortStatusNotUP."""
+
+    def __init__(self, evc_id: str, msg: str) -> None:
+        """Constructor of ProxyPortNotUP."""
+        super().__init__(evc_id, msg)
+
+
+class EVCHasNoINT(ErrorBase):
+    """Exception in case the EVC doesn't have INT enabled."""
+
+    def __init__(self, evc_id, message="INT isn't enabled"):
+        super().__init__(evc_id, message)
+
+
+class EVCHasINT(ErrorBase):
+    """Exception in case the EVC already has INT enabled."""
+
+    def __init__(self, evc_id, message="INT is already enabled"):
+        super().__init__(evc_id, message)
+
+
+class EVCNotFound(ErrorBase):
+    """Exception in case the EVC isn't found."""
+
+    def __init__(self, evc_id, message="not found"):
         super().__init__(evc_id, message)
 
 
 class FlowsNotFound(ErrorBase):
-    """Exception in case the EVC's flows are not there. Used for testing and debugging.
-    That's an warning. Maybe it requires some investigation."""
+    """Exception in case the EVC's flows are not there."""
 
-    def __init__(self, evc_id, message="Flow not found."):
+    def __init__(self, evc_id, message="flows not found"):
+        super().__init__(evc_id, message)
+
+
+class PriorityOverflow(ErrorBase):
+    """Exception in case the EVC's can't set a higher priority."""
+
+    def __init__(self, evc_id, message="setting a higher priority would overflow"):
         super().__init__(evc_id, message)
