6fff3f715be3658932eeda8dcc1efd83c9162057
Vinicius Arcanjo <vindasil@fiu.edu>
2023-08-26 16:26:17 -0300

    updated BATCH_INTERVAL to 0.2 and BATCH_SIZE to 200 until final tuning issue 40; 1k flows/sec for now

--- settings.py
+++ settings.py
@@ -12,9 +12,9 @@ UDP = 17
 
 # BATCH_INTERVAL: time interval between batch requests that will be sent to
 # flow_manager (in seconds) - zero enable sending all the requests in a row
-BATCH_INTERVAL = 0.5
+BATCH_INTERVAL = 0.2
 
 # BATCH_SIZE: size of a batch request that will be sent to flow_manager, in
 # number of FlowMod requests. Use 0 (zero) to disable BATCH mode, i.e. sends
 # everything at a glance
-BATCH_SIZE = 50
+BATCH_SIZE = 200
