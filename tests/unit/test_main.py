87a512d789c374ffbbf6c944572fd9b0789bb6c1
Vinicius Arcanjo <vindasil@fiu.edu>
2024-05-16 13:57:17 -0300

    chore: refactored unit tests accordingly

--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -257,21 +257,21 @@ class TestMain:
 
     async def test_on_evc_deployed(self) -> None:
         """Test on_evc_deployed."""
-        content = {"metadata": {"telemetry_request": {}}, "evc_id": "some_id"}
+        content = {"metadata": {"telemetry_request": {}}, "id": "some_id"}
         self.napp.int_manager.redeploy_int = AsyncMock()
         self.napp.int_manager.enable_int = AsyncMock()
         await self.napp.on_evc_deployed(KytosEvent(content=content))
         assert self.napp.int_manager.enable_int.call_count == 1
         assert self.napp.int_manager.redeploy_int.call_count == 0
 
-        content = {"metadata": {"telemetry": {"enabled": True}}, "evc_id": "some_id"}
+        content = {"metadata": {"telemetry": {"enabled": True}}, "id": "some_id"}
         await self.napp.on_evc_deployed(KytosEvent(content=content))
         assert self.napp.int_manager.enable_int.call_count == 1
         assert self.napp.int_manager.redeploy_int.call_count == 1
 
     async def test_on_evc_deleted(self) -> None:
         """Test on_evc_deleted."""
-        content = {"metadata": {"telemetry": {"enabled": True}}, "evc_id": "some_id"}
+        content = {"metadata": {"telemetry": {"enabled": True}}, "id": "some_id"}
         self.napp.int_manager.disable_int = AsyncMock()
         await self.napp.on_evc_deleted(KytosEvent(content=content))
         assert self.napp.int_manager.disable_int.call_count == 1
@@ -285,7 +285,7 @@ class TestMain:
         )
         content = {
             "metadata": {"telemetry": {"enabled": True}},
-            "evc_id": "some_id",
+            "id": "some_id",
             "active": True,
         }
         await self.napp.on_uni_active_updated(KytosEvent(content=content))
@@ -304,7 +304,7 @@ class TestMain:
         content = {
             "enabled": False,
             "metadata": {"telemetry": {"enabled": False}},
-            "evc_id": "some_id",
+            "id": "some_id",
         }
         self.napp.int_manager.remove_int_flows = AsyncMock()
         await self.napp.on_evc_undeployed(KytosEvent(content=content))
@@ -319,7 +319,7 @@ class TestMain:
         content = {
             "enabled": True,
             "metadata": {"telemetry": {"enabled": False}},
-            "evc_id": "some_id",
+            "id": "some_id",
         }
         self.napp.int_manager.redeploy_int = AsyncMock()
         await self.napp.on_evc_redeployed_link(KytosEvent(content=content))
@@ -334,7 +334,7 @@ class TestMain:
         content = {
             "enabled": True,
             "metadata": {"telemetry": {"enabled": False}},
-            "evc_id": "some_id",
+            "id": "some_id",
         }
         self.napp.int_manager.remove_int_flows = AsyncMock()
         await self.napp.on_evc_error_redeployed_link_down(KytosEvent(content=content))
